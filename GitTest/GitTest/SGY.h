a8d72b0acd06a22a46eb0b1ddb7ad0f83831b989
sungy <sungy@uway.cn>
2017-06-12 19:41:13 +0800

    2017 19：41第三次修改

---
+++ GitTest/GitTest/SGY.h
@@ -0,0 +1,13 @@
+//
+//  SGY.h
+//  GitTest
+//
+//  Created by SunGuoYan on 2017/6/12.
+//  Copyright © 2017年 SunGuoYan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGY : UIView
+
+@end
---
+++ GitTest/GitTest/SGY.m
@@ -0,0 +1,15 @@
+//
+//  SGY.m
+//  GitTest
+//
+//  Created by SunGuoYan on 2017/6/12.
+//  Copyright © 2017年 SunGuoYan. All rights reserved.
+//
+
+#import "SGY.h"
+
+@implementation SGY
+
+//是真的吗？
+
+@end
